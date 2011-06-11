0ab1d5b83be3c8854aaa9e9074e716aedf63ae8f
Clement Moussu <moussu@telecom-paristech.fr>
2011-06-11 18:35:37 +0200

    simulator: ping message 'pPORT' creates a new block and PORT is used to answer.

--- source/simulator/src/main/scala/server.scala
+++ source/simulator/src/main/scala/server.scala
@@ -14,7 +14,7 @@ package simulator.ether {
     val buffer  = new Array[Byte](bufsize)
     val socket  = new DatagramSocket(port)
     val packet  = new DatagramPacket(buffer, bufsize)
-    var portsToBlocks    = Map[Int, Block]()
+    var portsToBlocks  = Map[Int, Block]()
     var blocksToPorts  = Map[Block, Int]()
 
     println ("UDP server started: localhost:" + port)
@@ -25,30 +25,22 @@ package simulator.ether {
 
         lock.acquire
 
-        portsToBlocks.find(e => e._1 == packet.getPort()) match {
-          case None => {
-            val block = pool.addBlock()
-            portsToBlocks += ((packet.getPort(), block))
-            blocksToPorts += ((block, packet.getPort()))
-            println("New client {port:" + packet.getPort() +
-                    ", block:" + block + "}")
-          }
-          case _ => ()
-        }
-
-        lock.release
-
-        pool.refresh()
-
-        lock.acquire
-
         val packetCmd = packet.getData.head
         val packetData: Array[Byte] = Array.ofDim(packet.getLength - 1)
         Array.copy(packet.getData, 1, packetData, 0, packet.getLength - 1)
         val packetDataInt = packetData map { x => x.asInstanceOf[Int] + 128 }
 
         packetCmd match {
-          case 'p' => {}
+          case 'p' => {
+            val answerPort = new String(packetData.tail).toInt
+            val block = pool.addBlock()
+            portsToBlocks = portsToBlocks.updated(packet.getPort, block)
+            blocksToPorts = blocksToPorts.updated(block, answerPort)
+            println("New client {port:" + answerPort +
+                    ", block:" + block + "}")
+            lock.release
+            pool.refresh
+          }
           case 'm' => {
             val data = packetData.tail
             val ir = Dir.fromChar(packetData(0).toChar)
@@ -61,6 +53,7 @@ package simulator.ether {
               }
               case None => {}
             }
+            lock.release
           }
           case 'l' => {
             val i = packetDataInt(0)
@@ -69,10 +62,10 @@ package simulator.ether {
             val g = packetDataInt(3)
             val b = packetDataInt(4)
             portsToBlocks(packet.getPort()).leds(i)(j).setColor(r, g, b)
+            lock.release
           }
+          case _ => lock.release
         }
-
-        lock.release
       }
     }
 
