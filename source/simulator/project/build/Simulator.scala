b7f690cab552c26d2c69acdee48fc2648ab411f9
Clément Moussu <moussu@telecom-paristech.fr>
2011-06-23 16:09:48 +0200

    simulator: increase heap size to avoid java.lang.OutOfMemoryError.

--- source/simulator/project/build/Simulator.scala
+++ source/simulator/project/build/Simulator.scala
@@ -1,4 +1,9 @@
 import sbt._
 import spde._
 
-class Simulator(info: ProjectInfo) extends DefaultSpdeProject(info)
+class Simulator(info: ProjectInfo) extends DefaultSpdeProject(info) {
+  override def fork = Some(new ForkScalaRun {
+        override def runJVMOptions = super.runJVMOptions ++ Seq("-Xmx512m")
+        override def scalaJars = Seq(buildLibraryJar.asFile, buildCompilerJar.asFile)
+  })
+}
