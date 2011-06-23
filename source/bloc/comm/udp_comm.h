d794bbcc63083eefb07004c5158c58e944275829
Clément Moussu <moussu@telecom-paristech.fr>
2011-06-23 16:08:59 +0200

    bloc: fix bug when EINTR during recvfrom, redo.

--- source/bloc/comm/udp_comm.h
+++ source/bloc/comm/udp_comm.h
@@ -3,6 +3,10 @@
 
 # include <sys/socket.h>
 # include <netinet/in.h>
+# include <errno.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
 
 struct udp_comm_t
 {
@@ -18,8 +22,15 @@ udp_client_init(struct udp_comm_t* c, const char* addr, int port);
 inline void
 udp_client_send(struct udp_comm_t* c, const char* message, int size)
 {
-  sendto(c->socket, message, size, 0,
-         (struct sockaddr*)&(c->addr), c->addr_size);
+  int retcode = sendto(c->socket, message, size, 0,
+                       (struct sockaddr*)&(c->addr), c->addr_size);
+
+  if (retcode == -1)
+  {
+    fprintf(stderr, "Error %d in recvfrom: %s\n",
+            errno, strerror(errno));
+    exit(errno);
+  }
 }
 
 void
@@ -28,8 +39,29 @@ udp_server_init(struct udp_comm_t* c, const char* addr, int port);
 inline int
 udp_server_recv(struct udp_comm_t* c, char* message, int max_size)
 {
-  return recvfrom(c->socket, message, max_size, 0,
-                  (struct sockaddr*)&(c->addr), &c->addr_size);
+  int retcode;
+
+  for (;;)
+  {
+    retcode = recvfrom(c->socket, message, max_size, 0,
+             (struct sockaddr*)&(c->addr), &c->addr_size);
+
+    if (retcode > -1)
+      break;
+
+    switch (errno)
+    {
+      case EINTR:
+        break;
+      default:
+        fprintf(stderr, "Error %d in recvfrom: %s\n",
+                errno, strerror(errno));
+        exit(errno);
+    }
+  }
+
+
+  return retcode;
 }
 
 #endif
