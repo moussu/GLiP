c30e51cfb9a8b605fdd4a4436a1bba51e0671994
Clément Moussu <moussu@telecom-paristech.fr>
2011-07-03 16:25:57 +0200

    image: introduce lib_image by MBLed, adapt coding style and functionalities.

--- source/bloc/comm/simulator.h
+++ source/bloc/comm/simulator.h
@@ -2,6 +2,7 @@
 # define SIMULATOR_H
 
 # include "olsr/olsr_ifaces.h"
+# include "image/image.h"
 # include "udp_comm.h"
 
 # define SIMULATOR_PORT 4444
@@ -13,12 +14,6 @@ typedef struct
   struct udp_comm_t server;
 } simulator_t;
 
-typedef unsigned short pixel_t;
-typedef pixel_t* image_t;
-#define R(rgb) (17 * ((rgb >> 8) & 0xf))
-#define G(rgb) (17 * ((rgb >> 4) & 0xf))
-#define B(rgb) (17 * ((rgb >> 0) & 0xf))
-#define PIXEL(Image, I, J) (Image[(I) * 8 + (J)])
 
 int simulator_init(int server_port, socket_callback_t socket);
 void simulator_send(const char* message, int size, interface_t iface);
