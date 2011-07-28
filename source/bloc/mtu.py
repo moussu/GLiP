546421289ccadb4a0e3a760dc9e99c923e77d812
Clément Moussu <moussu@telecom-paristech.fr>
2011-07-28 22:20:06 +0200

    bloc: make mtu script python 2/3 compatible.

--- source/bloc/mtu.py
+++ source/bloc/mtu.py
@@ -3,12 +3,12 @@
 import sys
 
 if len(sys.argv) != 2:
-    print "usage: %s MTU" % sys.argv[0]
+    print("usage: %s MTU" % sys.argv[0])
     exit(1)
 
-print "#ifndef MTU_H"
-print "#define MTU_H"
-print "# define MTU %s" % sys.argv[1]
-print "#endif"
+print("#ifndef MTU_H")
+print("#define MTU_H")
+print("# define MTU %s" % sys.argv[1])
+print("#endif")
 
 exit(0)
