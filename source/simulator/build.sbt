a53000472a0b2137308b4b429fbe8eafc3ce0072
Clement Moussu <clement.moussu@gmail.com>
2012-01-03 20:53:47 -0800

    Make simulator work with sbt 0.11.2-1.
    	* Get rid of spde (frozen sbt compatibility)
    	* Get rid of old sbt build files
    	* Create build.sbt
    	* Put processing jars in lib/

---
+++ source/simulator/build.sbt
@@ -0,0 +1,3 @@
+name := "GLiP Simulator"
+
+version := "1.0.0"
--- source/simulator/project/build/Simulator.scala
+++
@@ -1,9 +0,0 @@
-import sbt._
-import spde._
-
-class Simulator(info: ProjectInfo) extends DefaultSpdeProject(info) {
-  override def fork = Some(new ForkScalaRun {
-        override def runJVMOptions = super.runJVMOptions ++ Seq("-Xmx512m")
-        override def scalaJars = Seq(buildLibraryJar.asFile, buildCompilerJar.asFile)
-  })
-}
